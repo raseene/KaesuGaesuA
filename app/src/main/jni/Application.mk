8fb315cd18435e6f91145f34fa4e335939401e95
raseene <so.raseene.68@gmail.com>
2018-11-01 23:38:25 +0900

    ・externalNativeBuildに変更 ・NDK r18bに対応

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,12 +1,1 @@
-#APP_OPTIM := debug
-APP_OPTIM := release
-
-ifeq ($(APP_OPTIM),debug)
-  APP_ABI := armeabi-v7a
-else
-  APP_ABI := armeabi armeabi-v7a
-endif
-APP_PLATFORM := android-14
-APP_STL := gnustl_static
-STLPORT_FORCE_REBUILD := true
-APP_CFLAGS += -Ofast
+#APP_CFLAGS += -Ofast
